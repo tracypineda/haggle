aa58ff3f798ae955ceb84283b278d92b50a32f3e
Tracy Pineda <tracypineda@gmail.com>
2019-11-10 12:27:33 -0700

    adding nodemailer routes

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -6,8 +6,10 @@ function Footer() {
         <footer className="footer">
             <p><i className="fab fa-react react-icon"></i></p>
             <p>Tracy Pineda and April Long</p>
-            <p><a id="githib" href="github.com/tracypineda" target="blank"><i className="fab fa-github gh-icon"></i></a></p>
-            <p><a id="githib" href="github.com/ApVen77" target="blank"><i className="fab fa-github gh-icon"></i></a></p>
+            <p><a id="githib" href="https://github.com/tracypineda/haggle" target="blank"><i className="fab fa-github gh-icon"></i></a></p>
+
+            {/* <p><a id="githib" href="github.com/ApVen77" target="blank"><i className="fab fa-github gh-icon"></i></a></p> */}
+            
         </footer>
     )
 }
