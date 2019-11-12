c10a3c21b8351a052b9aee81b8a1459e227549f3
Tracy Pineda <tracypineda@gmail.com>
2019-11-12 14:40:15 -0700

    pulling april's changes

--- client/src/pages/Barter/index.js
+++ client/src/pages/Barter/index.js
@@ -1,8 +1,29 @@
 
+// // import React, {Component} from "react";
+// // import "./Barter.scss";
+// // import { Button } from "reactstrap";
+// // import { Link } from "react-router-dom"
+// // import API from '../Auth';
+
+// // class Barter extends Component {
+// //     state = {
+// //         loggedIn: false,
+// //         user: null,
+// //         loading: true,
+// //         wants: "",
+// //         haves: "",
+// //         itemName: "",
+// //         itemDescription: ""
+// //     }
+
+// //     componentDidMount() {
+
+// //         this.loading();
+
 // import React, {Component} from "react";
 // import "./Barter.scss";
 // import { Button } from "reactstrap";
-// import { Link } from "react-router-dom"
+// import { Link } from "react-router-dom";
 // import API from '../Auth';
 
 // class Barter extends Component {
@@ -10,40 +31,19 @@
 //         loggedIn: false,
 //         user: null,
 //         loading: true,
-//         wants: "",
-//         haves: "",
-//         itemName: "",
-//         itemDescription: ""
+//         itemName:"",
+//         itemDescription:""
 //     }
 
-//     componentDidMount() {
-
-//         this.loading();
-
-import React, {Component} from "react";
-import "./Barter.scss";
-import { Button } from "reactstrap";
-import { Link } from "react-router-dom";
-import API from '../Auth';
-
-class Barter extends Component {
-    state = {
-        loggedIn: false,
-        user: null,
-        loading: true,
-        itemName:"",
-        itemDescription:""
-    }
-
    
-      loadHaves = () => {
-        API.getHaves()
-          .then(res =>
-            this.setState({ haves: res.data, itemName: "", itemDescription: "" })
-          )
-          .catch(err => console.log(err));
-      };
+//       loadHaves = () => {
+//         API.getHaves()
+//           .then(res =>
+//             this.setState({ haves: res.data, itemName: "", itemDescription: "" })
+//           )
+//           .catch(err => console.log(err));
+//       };
 
-    componentDidMount() {
-        this.loadHaves();
-        this.loading();
+//     componentDidMount() {
+//         // this.loadHaves();
+//         this.loading();
