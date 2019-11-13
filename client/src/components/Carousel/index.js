66edcf17b440da8fb5695a613eed83a90ad9f956
April Long <april11dance@gmail.com>
2019-11-13 14:15:21 -0700

    making changes to barter.js and carousel

--- client/src/components/Carousel/index.js
+++ client/src/components/Carousel/index.js
@@ -10,27 +10,27 @@ class MyCarousel extends Component {
     render() {
         return (
             <Carousel showArrows={true} onChange={onChange} onClickItem={onClickItem}>
-                <div style={{height:"200px"}}>
+                <div style={{height:"200px", width: "100px"}}>
                     <img src="https://i.pinimg.com/originals/ed/76/f7/ed76f7eef192e4a0b1fc66a3aa1f2c00.png" />
                     <p className="legend">Legend 1</p>
                 </div>
-                <div style={{height:"200px"}}>
+                <div style={{height:"200px", width: "100px"}}>
                     <img src="https://i.pinimg.com/originals/ed/76/f7/ed76f7eef192e4a0b1fc66a3aa1f2c00.png" />
                     <p className="legend">Legend 2</p>
                 </div>
-                <div style={{height:"200px"}}>
+                <div style={{height:"200px", width: "100px"}}>
                     <img  src="https://www.duffyduffylaw.com/wp-content/uploads/2018/05/user-placeholder-200x250.jpg" />
                     <p className="legend">Legend 3</p>
                 </div>
-                <div style={{height:"200px"}}>
+                <div style={{height:"200px", width: "100px"}}>
                     <img  src="https://www.duffyduffylaw.com/wp-content/uploads/2018/05/user-placeholder-200x250.jpg" />
                     <p className="legend">Legend 4</p>
                 </div>
-                <div style={{height:"200px"}}>
+                <div style={{height:"200px", width: "100px"}}>
                     <img  src="https://static5.depositphotos.com/1000797/453/v/450/depositphotos_4532300-stock-illustration-woman-in-red.jpg" />
                     <p className="legend">Legend 5</p>
                 </div>
-                <div style={{height:"200px"}}>
+                <div style={{height:"200px", width: "100px"}}>
                     <img  src="https://static5.depositphotos.com/1000797/453/v/450/depositphotos_4532300-stock-illustration-woman-in-red.jpg" />
                     <p className="legend">Legend 6</p>
                 </div>
