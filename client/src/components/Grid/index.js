5be344801cbf962314c07ea9c9e836b4c1aa0a92
Tracy Pineda <tracypineda@gmail.com>
2019-11-13 15:06:38 -0700

    barter and profile page up and running

--- client/src/components/Grid/index.js
+++ client/src/components/Grid/index.js
@@ -14,15 +14,15 @@ export function Row({ fluid, children }) {
 
 // This Col component lets us size bootstrap columns with less syntax
 // e.g. <Col size="md-12"> instead of <div className="col-md-12">
-export function Col({ size, children }) {
-  return (
-    <div
-      className={size
-        .split(" ")
-        .map(size => "col-" + size)
-        .join(" ")}
-    >
-      {children}
-    </div>
-  );
-}
+// export function Col({ size, children }) {
+//   return (
+//     <div
+//       className={size
+//         .split(" ")
+//         .map(size => "col-" + size)
+//         .join(" ")}
+//     >
+//       {children}
+//     </div>
+//   );
+// }
